36fa082768cc82227870e19077c371c53ff01cf1
EugeneJay9601 <eugenejay.tesnado@gsfe.tupcavite.edu.ph>
2022-11-28 13:50:25 -0300

    sas

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Plex from "../components/Plex";
+
+
+export default function PLEX() {
+  return (
+    <div> 
+
+      <Plex
+        heading1="Researcher’s Library"
+        pAddress="Search thousands of research and thesis papers for your knowledge. "/>
+
+    </div>
+  );
+};
